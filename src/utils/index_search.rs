a6824723ff88eb9833fd9a0d9d041d552a666dae
Gus Garcia <guscastles@gmail.com>
2022-11-05 17:19:57 +1100

    Simplify code

--- src/utils/index_search.rs
+++
@@ -1,3 +0,0 @@
-pub fn find_parent(child: usize) -> usize {
-    (child - (2 - child % 2)) / 2
-}
--- src/utils/transform.rs
+++
@@ -1,3 +0,0 @@
-pub fn swap(elements: &mut Vec<u32>, indexes: (usize, usize)) {
-    (elements[indexes.1], elements[indexes.0]) = (elements[indexes.0], elements[indexes.1]);
-}
