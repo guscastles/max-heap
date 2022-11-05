085132cfe4ad1e7115dc27ec53e57faa1f4a89dc
Gus Garcia <guscastles@gmail.com>
2022-11-05 22:19:39 +1100

    Update name of variable

--- src/test_min_heap.rs
+++ src/test_min_heap.rs
@@ -1,10 +1,10 @@
-use super::engine::*;
+use super::engine::{heapify, MIN_HEAP};
 
 #[test]
 fn heapify_with_minimum_priority_queue() {
     let mut elements = vec![1, 2, 5, 1, 4, 0, 10, 9, 8, 100];
     heapify(&mut elements, MIN_HEAP);
-    let max_element = elements[0];
-    assert_eq!(max_element, 0);
+    let min_element = elements[0];
+    assert_eq!(min_element, 0);
     assert_eq!(elements, vec![0, 1, 1, 2, 4, 5, 10, 9, 8, 100]);
 }
