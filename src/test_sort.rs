c68059532b46cd4fc8f7dfb0e69d4ce88f749c45
Gus Garcia <guscastles@gmail.com>
2022-11-07 05:31:43 +1100

    Add bench test for sort with 10 elements

--- src/test_sort.rs
+++ src/test_sort.rs
@@ -24,3 +24,11 @@ fn sort_large_vector(bencher: &mut Bencher) {
         sort(&mut elements, MAX_HEAP)
     });
 }
+
+#[bench]
+fn sort_with_10_elements(bencher: &mut Bencher) {
+    bencher.iter(|| {
+        let mut elements = test::black_box(vec![1, 2, 5, 1, 4, 0, 10, 9, 8, 100]);
+        sort(&mut elements, MAX_HEAP)
+    });
+}
